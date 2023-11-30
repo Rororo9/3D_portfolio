6c55a87ef308c8cab5dc9b035f8507eab16847f4
“Vadim” <freegasss@gmail.com>
2023-12-01 02:58:06 +1100

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/3D_portfolio/about",
+  base:"/3D_portfolio/about/",
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
